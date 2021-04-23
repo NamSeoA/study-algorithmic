becaf1f63ba3fbf8aca3d3f2e801c5c1b8a8dbdf
NamSeoA <71997900+NamSeoA@users.noreply.github.com>
2021-04-23 17:52:40 +0900

    4월 22일 _문자열 2908
    두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.

--- Baekjoon/src/basic/practice5.java
+++ Baekjoon/src/basic/practice5.java
@@ -130,7 +130,6 @@ public class practice5 {
 		 첫 줄에 영어 대소문자와 띄어쓰기로 이루어진 문자열이 주어진다. 이 문자열의 길이는 1,000,000을 넘지 않는다. 단어는 띄어쓰기 한 개로 구분되며, 공백이 연속해서 나오는 경우는 없다. 
 		 또한 문자열의 앞과 뒤에는 공백이 있을 수도 있다.
 		 첫째 줄에 단어의 개수를 출력한다.
-		 */
 				
 		String words = sc.nextLine(); //문장 입력 받기 
 		
@@ -141,12 +140,40 @@ public class practice5 {
 		}else {
 			System.out.println("0");
 		}
-				
+		*/		
 		
 		
 		
+		// 2908
+		/*
+		 상근이의 동생 상수는 수학을 정말 못한다. 상수는 숫자를 읽는데 문제가 있다. 이렇게 수학을 못하는 상수를 위해서 상근이는 수의 크기를 비교하는 문제를 내주었다. 
+		 상근이는 세 자리 수 두 개를 칠판에 써주었다. 그 다음에 크기가 큰 수를 말해보라고 했다.
+		 상수는 수를 다른 사람과 다르게 거꾸로 읽는다. 예를 들어, 734와 893을 칠판에 적었다면, 상수는 이 수를 437과 398로 읽는다. 
+		 따라서, 상수는 두 수중 큰 수인 437을 큰 수라고 말할 것이다.
+		 두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.
+		 첫째 줄에 상근이가 칠판에 적은 두 수 A와 B가 주어진다. 두 수는 같지 않은 세 자리 수이며, 0이 포함되어 있지 않다.
+		 첫째 줄에 상수의 대답을 출력한다.
+		 */
 		
+		/* 풀이 
+		 먼저 StringBuilder 는 문자열을 다루는 클래스로 많이 쓰이고 있는데, 여기서 reverse() 라는 아주 좋은 메소드를 포함하고 있다.
+	     즉, 위를 사용하기 위해 먼저 StringBuilder 생성과 동시에 append() 라는 메소드에 값을 넣어준다.
+	    이때 append() 로 넣어진 값은 타입이 StringBuilder 라는 타입으로 변환된다.
+		그리고 그렇게 저장된 수를 reverse() 라는 메소드를 통해 저장되어있던 문자열을 뒤집는다. 그리고 StringBuilder 타입을 문자열로 반환시키기 위해 toString() 을 써주면 끝이다.
+        그리고 문자열로 반환시켰으니 Integer.parseInt() 로 String 을 int 로 타입을 변경시키기 
+		*/
 		
+		int a = sc.nextInt(); 
+		int b = sc.nextInt();
+		
+		a = Integer.parseInt(new StringBuilder().append(a).reverse().toString());
+		b = Integer.parseInt(new StringBuilder().append(b).reverse().toString());
+		
+		if(a>b) {
+			System.out.println("더 큰 수는 : "+a);
+		}else {
+			System.out.println("더 큰 수는 : "+b);
+		}
 		
 		
 		
