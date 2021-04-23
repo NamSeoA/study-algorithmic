bb4281f55d294caba48884bd84b4393cbf5a27b0
NamSeoA <71997900+NamSeoA@users.noreply.github.com>
2021-04-23 17:53:47 +0900

    4월 23일 _문자열 5622
    첫째 줄에 다이얼을 걸기 위해서 필요한 최소 시간을 출력한다.

--- Baekjoon/src/basic/practice5.java
+++ Baekjoon/src/basic/practice5.java
@@ -161,7 +161,7 @@ public class practice5 {
 	    이때 append() 로 넣어진 값은 타입이 StringBuilder 라는 타입으로 변환된다.
 		그리고 그렇게 저장된 수를 reverse() 라는 메소드를 통해 저장되어있던 문자열을 뒤집는다. 그리고 StringBuilder 타입을 문자열로 반환시키기 위해 toString() 을 써주면 끝이다.
         그리고 문자열로 반환시켰으니 Integer.parseInt() 로 String 을 int 로 타입을 변경시키기 
-		*/
+		
 		
 		int a = sc.nextInt(); 
 		int b = sc.nextInt();
@@ -174,6 +174,64 @@ public class practice5 {
 		}else {
 			System.out.println("더 큰 수는 : "+b);
 		}
+		*/
+		
+
+		// 5622
+		/*
+		전화를 걸고 싶은 번호가 있다면, 숫자를 하나를 누른 다음에 금속 핀이 있는 곳 까지 시계방향으로 돌려야 한다. 숫자를 하나 누르면 다이얼이 처음 위치로 돌아가고, 다음 숫자를 누르려면 다이얼을 처음 위치에서 다시 돌려야 한다.
+		숫자 1을 걸려면 총 2초가 필요하다. 1보다 큰 수를 거는데 걸리는 시간은 이보다 더 걸리며, 한 칸 옆에 있는 숫자를 걸기 위해선 1초씩 더 걸린다.
+		상근이의 할머니는 전화 번호를 각 숫자에 해당하는 문자로 외운다. 즉, 어떤 단어를 걸 때, 각 알파벳에 해당하는 숫자를 걸면 된다. 예를 들어, UNUCIC는 868242와 같다.
+		할머니가 외운 단어가 주어졌을 때, 이 전화를 걸기 위해서 필요한 최소 시간을 구하는 프로그램을 작성하시오.
+		첫째 줄에 알파벳 대문자로 이루어진 단어가 주어진다. 단어의 길이는 2보다 크거나 같고, 15보다 작거나 같다.
+		첫째 줄에 다이얼을 걸기 위해서 필요한 최소 시간을 출력한다.
+		 */
+		
+		// 문자열, 시간 카운팅 
+		// 문자열 입력받음, abc cdf .. 이렇게 나눠서 시간 더해주기
+		
+		String numbereng = sc.next(); //번호 -> 문자 
+		
+		int time = 0; // 걸리는 시간 
+		int k = numbereng.length(); // int로 길이
+		
+		for(int i=0; i<k; i++) {
+			
+			switch (numbereng.charAt(i)) {
+			case 'A' : case 'B': case 'C' : // char '' 사
+				time += 3;
+				break;
+			case 'D' : case 'E': case 'F' :
+				time += 4; 
+				break;
+                
+			case 'G' : case 'H': case 'I' :
+				time += 5; 
+				break;
+                
+			case 'J' : case 'K': case 'L' : 
+				time += 6;
+				break;
+                
+			case 'M' : case 'N': case 'O' :
+				time += 7;
+				break;
+                
+			case 'P' : case 'Q': case 'R' : case 'S' : 
+				time += 8; 
+				break;
+                
+			case 'T' : case 'U': case 'V' : 
+				time += 9;
+				break;
+                
+			case 'W' : case 'X': case 'Y' : case 'Z' : 
+				time += 10;
+				break;
+			}
+		}
+		System.out.println("번호 누르는데 걸리는 시간 : "+time);
+		
 		
 		
 		
